0a3084a8eec002f8d37dc998d6cab2028c6f4cb6
Alireza Saghaeian <s.saghaeian@student.tudelft.nl>
2026-02-18 12:21:15 +0100

    add safe mode and control trait implementation for that

--- dronecode/src/control.rs
+++ dronecode/src/control.rs
@@ -1,5 +1,8 @@
+use crate::control_trait::FSMControl;
 use crate::yaw_pitch_roll::YawPitchRoll;
 use alloc::format;
+use my_hdlc::command::{Command, CommandType, FSMState};
+use my_hdlc::HdlcTransceiver;
 use tudelft_quadrupel::barometer::read_pressure;
 use tudelft_quadrupel::battery::read_battery;
 use tudelft_quadrupel::block;
@@ -7,44 +10,61 @@ use tudelft_quadrupel::led::Led::Blue;
 use tudelft_quadrupel::motor::get_motors;
 use tudelft_quadrupel::mpu::{read_dmp_bytes, read_raw};
 use tudelft_quadrupel::time::{set_tick_frequency, wait_for_next_tick, Instant};
-use tudelft_quadrupel::uart::send_bytes;
+use tudelft_quadrupel::uart::{receive_bytes, send_bytes};
 
-pub fn control_loop() -> ! {
+const UART_BUF_SIZE: usize = 255usize;
+
+pub fn main_loop() -> ! {
     set_tick_frequency(100);
     let mut last = Instant::now();
-
+    let mut op_mode = FSMState::SafeMode;
+    let mut uart_buf = [0u8; UART_BUF_SIZE];
+    let mut transceiver: HdlcTransceiver = HdlcTransceiver::new();
     for i in 0.. {
         let _ = Blue.toggle();
         let now = Instant::now();
         let dt = now.duration_since(last);
         last = now;
 
-        let motors = get_motors();
-        let quaternion = block!(read_dmp_bytes()).unwrap();
-        let ypr = YawPitchRoll::from(quaternion);
-        let (accel, _) = read_raw().unwrap();
-        let bat = read_battery();
-        let pres = read_pressure();
-
-        if i % 100 == 0 {
-            send_bytes(format!("DTT: {:?}ms\n", dt.as_millis()).as_bytes());
-            send_bytes(
-                format!(
-                    "MTR: {} {} {} {}\n",
-                    motors[0], motors[1], motors[2], motors[3]
-                )
-                .as_bytes(),
-            );
-            send_bytes(format!("YPR {} {} {}\n", ypr.yaw, ypr.pitch, ypr.roll).as_bytes());
-            send_bytes(format!("ACC {} {} {}\n", accel.x, accel.y, accel.z).as_bytes());
-            send_bytes(format!("BAT {bat}\n").as_bytes());
-            send_bytes(format!("BAR {pres}\n").as_bytes());
-            send_bytes("\n".as_bytes());
+        // Read Uart Buff
+        let num_received = receive_bytes(&mut uart_buf);
+        if num_received != 0usize {
+            transceiver.add_bytes(&uart_buf[..num_received]);
+            let deserialized_command =
+                transceiver.read_structure::<my_hdlc::command::CommandType>();
+            if let Some(command) = deserialized_command {
+                run_command(command);
+            }
         }
 
+        // control_loop(op_mode);
+        op_mode.run_control_loop();
+        if i % 100 == 0 {
+            // send_drone_data();
+        }
+
+        // Control Loop:
+        // Read Command and Execute, (if available).
+        // Run the current mode's control loop
+        // send data if i%100 == 0
         // wait until the timer interrupt goes off again
         // based on the frequency set above
+
         wait_for_next_tick();
     }
     unreachable!();
+}
+
+fn run_command(command: CommandType) {
+    todo!("Execute Commands!");
+}
+
+fn send_drone_data() {
+    let motors = get_motors();
+    let quaternion = block!(read_dmp_bytes()).unwrap();
+    let ypr = YawPitchRoll::from(quaternion);
+    let (accel, _) = read_raw().unwrap();
+    let bat = read_battery();
+    let pres = read_pressure();
+    todo!("Put the data in a struct and send it!");
 }
