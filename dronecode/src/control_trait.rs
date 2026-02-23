12cf4a409d15a218e153cb6c2d9392266870c2d9
Alireza Saghaeian <S.saghaeian@student.tudelft.nl>
2026-02-23 21:23:23 +0100

    rework fsm control trait

--- dronecode/src/control_trait.rs
+++ dronecode/src/control_trait.rs
@@ -1,26 +1,27 @@
 use my_hdlc::command::FSMState;
-use tudelft_quadrupel::motor::*;
+
 pub trait FSMControl {
-    fn run_control_loop(&mut self);
-    fn run_safe_mode_cl(&mut self);
+    fn run_control_loop(&self);
+    // fn run_safe_mode_cl(& self);
+    fn step(&self, next_state: FSMState) -> &dyn FSMControl;
 }
 
-impl FSMControl for FSMState {
-    fn run_control_loop(&mut self) {
-        match self {
-            FSMState::SafeMode => self.run_safe_mode_cl(),
-            FSMState::CalibrationMode => todo!(),
-            FSMState::FullControlMode => todo!(),
-            FSMState::HeightControlMode => todo!(),
-            FSMState::ManualMode => todo!(),
-            FSMState::PanicMode => todo!(),
-            FSMState::RawSensorsFullControlMode => todo!(),
-            FSMState::WirelessMode => todo!(),
-            FSMState::YawControl => todo!(),
-        }
-    }
+// impl FSMControl for FSMState {
+//     fn run_control_loop(&mut self) {
+//         match self {
+//             FSMState::SafeMode => self.run_safe_mode_cl(),
+//             FSMState::CalibrationMode => todo!(),
+//             FSMState::FullControlMode => todo!(),
+//             FSMState::HeightControlMode => todo!(),
+//             FSMState::ManualMode => todo!(),
+//             FSMState::PanicMode => todo!(),
+//             FSMState::RawSensorsFullControlMode => todo!(),
+//             FSMState::WirelessMode => todo!(),
+//             FSMState::YawControl => todo!(),
+//         }
+//     }
 
-    fn run_safe_mode_cl(&mut self) {
-        set_motor_max(0);
-    }
-}
+//     fn run_safe_mode_cl(&mut self) {
+//         set_motor_max(0);
+//     }
+// }
