11ef2db22048227dbd2fff6eadda25db585a297d
Borislav Semerdzhiev <borislavsemerdzhiev.02@gmail.com>
2026-02-25 17:07:56 +0100

    tweak the values a bit

--- my-hdlc/src/lib.rs
+++ my-hdlc/src/lib.rs
@@ -12,8 +12,8 @@ use serde::{Deserialize, Serialize};
 * payload, and adding the two framing bytes, we can end up with at most (2*3 + 2)8 bytes. In total,
 * we can fit 128/8 ~ 16 messages in the buffer.
 */
-pub static BUFFER_SIZE: usize = 1 << 7;
-pub static MESSAGE_SIZE: usize = 1 << 4;
+pub static BUFFER_SIZE: usize = 1 << 8;
+pub static MESSAGE_SIZE: usize = 1 << 6;
 // used as return size when serializing a structure
 pub static STUFFED_MESSAGE_SIZE: usize = MESSAGE_SIZE * 2 + 2;
 
