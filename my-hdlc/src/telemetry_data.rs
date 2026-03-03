60d3094cc44e0ce86307c51b70a903a8ca66be92
Alireza Saghaeian <S.saghaeian@student.tudelft.nl>
2026-03-03 18:41:33 +0100

    Fix memory panic caused by small buffer

--- my-hdlc/src/telemetry_data.rs
+++ my-hdlc/src/telemetry_data.rs
@@ -3,16 +3,16 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct TelemetryData {
     pub dt: u128,
-    pub motors: [u16; 4],
-    pub yaw: f32,
-    pub pitch: f32,
-    pub roll: f32,
-    pub accel_x: i16,
-    pub accel_y: i16,
-    pub accel_z: i16,
-    pub gyro_x: i16,
-    pub gyro_y: i16,
-    pub gyro_z: i16,
+    // pub motors: [u16; 4],
+    // pub yaw: f32,
+    // pub pitch: f32,
+    // pub roll: f32,
+    // pub accel_x: i16,
+    // pub accel_y: i16,
+    // pub accel_z: i16,
+    // pub gyro_x: i16,
+    // pub gyro_y: i16,
+    // pub gyro_z: i16,
     pub bat: u16,
     pub pres: u32,
 }
@@ -28,23 +28,27 @@ impl fmt::Display for TelemetryData {
         // )
         write!(
             f,
-            "DTT: {:?}ms\n
-        MTR: {} {} {} {}\n
-        YPR {} {} {}\n
-        ACC {} {} {}\n
-        BAT {}\n
-        BAR {}\n",
+            //     "DTT: {:?}ms\n
+            // MTR: {} {} {} {}\n
+            // YPR {} {} {}\n
+            // ACC {} {} {}\n
+            // BAT {}\n
+            // BAR {}\n",
+            "
+            DTT: {:?}ms\n
+            BAT {}\n
+            BAR {}\n",
             self.dt,
-            self.motors[0],
-            self.motors[1],
-            self.motors[2],
-            self.motors[3],
-            self.yaw,
-            self.pitch,
-            self.roll,
-            self.accel_x,
-            self.accel_y,
-            self.accel_z,
+            // self.motors[0],
+            // self.motors[1],
+            // self.motors[2],
+            // self.motors[3],
+            // self.yaw,
+            // self.pitch,
+            // self.roll,
+            // self.accel_x,
+            // self.accel_y,
+            // self.accel_z,
             self.bat,
             self.pres
         )
