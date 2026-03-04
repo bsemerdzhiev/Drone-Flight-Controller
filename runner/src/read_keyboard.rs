ed560cf9bcae3c194e84392d18210f42ab74d752
Borislav Semerdzhiev <borislavsemerdzhiev.02@gmail.com>
2026-03-04 11:43:29 +0100

    add state transitions

--- runner/src/read_keyboard.rs
+++ runner/src/read_keyboard.rs
@@ -1,18 +1,52 @@
 use std::time::Duration;
 
 use crossterm::event::{self, Event};
-use my_hdlc::pc_command::ManualInput;
+use my_hdlc::{command::FSMState, pc_command::ManualInput};
 
-fn send_transition(state: my_hdlc::command::FSMState) {}
+fn send_transition(state: my_hdlc::command::FSMState) {
+    loop {
+        let send_buffer = rcv.write_structure::<my_hdlc::command::FSMState>(state);
+
+        serial.write(&send_buffer.0[0..send_buffer.1]);
+
+        let mut to_break = false;
+
+        //wait for ack
+        if let Ok(num) = serial.read(&mut buf[0..rcv.remaining_bytes]) {
+            rcv.add_bytes(&buf[0..num]);
+        }
+        if let Some(x) = rcv.read_structure::<my_hdlc::command::DeviceCommand>() {
+            match x {
+                my_hdlc::command::DeviceCommand::Ack => {
+                    to_break = true;
+                }
+                _ => {}
+            }
+            println!("{:?}\n", x);
+        }
+
+        if to_break {
+            break;
+        }
+    }
+}
 
 pub fn keyboard_trimming(keyboard_trim: &mut ManualInput, rcv: &mut my_hdlc::HdlcTransceiver) {
     while event::poll(Duration::from_millis(0)).unwrap() {
         if let Event::Key(key) = event::read().unwrap() {
             match key.code {
-                KeyCode::Char('0') => {}
-                KeyCode::Char('1') => {}
-                KeyCode::Char('2') => {}
-                KeyCode::Char('3') => {}
+                KeyCode::Char('0') => {
+                    send_transition(my_hdlc::command::FSMState::SafeMode);
+                }
+                KeyCode::Char('1') => {
+                    send_transition(my_hdlc::command::FSMState::PanicMode);
+                }
+                KeyCode::Char('2') => {
+                    send_transition(my_hdlc::command::FSMState::ManualMode);
+                }
+                KeyCode::Char('3') => {
+                    send_transition(my_hdlc::command::FSMState::CalibrationMode);
+                }
                 // Lift trim
                 // KeyCode::Char('a') => keyboard_trim.get_lift() += 0.01, //throttle up
                 // KeyCode::Char('z') => keyboard_trim.get_lift() -= 0.01, //throttle down
