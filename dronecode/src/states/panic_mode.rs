b2cd14f0aaf283882081a67ac67d7231228078c9
Borislav Semerdzhiev <borislavsemerdzhiev.02@gmail.com>
2026-03-10 22:06:16 +0100

    Change the code for drone descend

--- dronecode/src/states/panic_mode.rs
+++ dronecode/src/states/panic_mode.rs
@@ -18,22 +18,27 @@ impl FSMControl for FSMPanic {
         has_received_input: &mut bool,
         my_hdlc: &mut HdlcTransceiver,
     ) -> &dyn FSMControl {
-        let initial_speed = 100; // change as needed
         let current_speed = get_motors();
+        const DESCENT_STEP: u16 = 2;
 
-        if current_speed.iter().any(|&v| v > initial_speed) {
-            set_motors([initial_speed; 4]);
+        let mut avg_speed: u16 = 0;
+        for i in current_speed {
+            avg_speed += i;
+        }
+        avg_speed /= 4;
+
+        if current_speed.iter().any(|&v| v != avg_speed) {
+            set_motors([avg_speed; 4]);
             Red.on();
             return self;
         } else if current_speed[0] == 0 {
             Red.off();
             return &FSMSafe;
         } else {
-            // in the case the current maximum is smaller than initial speed
-            // equalize all motors and descend from there. Otherwise if motors were set
-            // to the initial speed previously it will just keep descending.
-            let max_value = current_speed.iter().copied().max().unwrap();
-            set_motors([(max_value.saturating_sub(1)); 4]);
+            // all motors are equalized
+            let new_speed: u16 = (avg_speed - DESCENT_STEP).max(0u16);
+
+            set_motors([new_speed; 4]);
             return self;
         }
     }
