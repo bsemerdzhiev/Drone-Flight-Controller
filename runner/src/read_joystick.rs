df09dfc08b8c189ba4a73e96a1bd785392bad15f
Borislav Semerdzhiev <borislavsemerdzhiev.02@gmail.com>
2026-03-11 12:49:21 +0100

    Fix orientation

--- runner/src/read_joystick.rs
+++ runner/src/read_joystick.rs
@@ -18,7 +18,7 @@ pub const MAX_LIFT: f32 = 10000f32;
 
 //------------------------------------------------------
 
-const YAW_RATE: f32 = 2000f32;
+const YAW_RATE: f32 = 200f32;
 const PITCH_RATE: f32 = 2000f32;
 const ROLL_RATE: f32 = 2000f32;
 
@@ -48,16 +48,17 @@ pub fn read_joystick(device: &mut Option<Device>, joystick_input: &mut ManualInp
                             }
                             AbsoluteAxisCode::ABS_X => {
                                 joystick_input
-                                    .set_roll((((v / 512.0) as f32 - 1.0) * ROLL_RATE) as i32);
+                                    .set_roll((((v / 512.0) - 1.0 as f32) * ROLL_RATE) as i32);
                             }
                             AbsoluteAxisCode::ABS_Y => {
                                 joystick_input
-                                    .set_pitch((((v / 512.0) as f32 - 1.0) * PITCH_RATE) as i32);
+                                    .set_pitch(((1.0 - (v / 512.0) as f32) * PITCH_RATE) as i32);
                             }
                             AbsoluteAxisCode::ABS_RZ => {
                                 // have to check what the standard value for this axis is
                                 joystick_input
                                     .set_yaw((((v / 128.0) as f32 - 1.0) * YAW_RATE) as i32);
+                                println!("{}\r", joystick_input.get_yaw());
                             }
                             _ => {}
                         }
