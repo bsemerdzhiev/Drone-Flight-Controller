7ed36146d8619fd1531b13bf70168b6bb0071cde
Alireza Saghaeian <S.saghaeian@student.tudelft.nl>
2026-03-18 11:54:08 +0100

    add ypr to calibration

--- dronecode/src/states/calibration_mode.rs
+++ dronecode/src/states/calibration_mode.rs
@@ -5,13 +5,15 @@ use crate::states::panic_mode::FSMPanic;
 use crate::states::state_structures::state_context::StateContext;
 use crate::states::yaw_control::FSMYaw;
 use crate::states::{fsm_base_class::FSMControl, safe_mode::FSMSafe};
+use crate::util::yaw_pitch_roll::YawPitchRoll;
 
 use alloc::boxed::Box;
 use my_hdlc::command::FSMState;
 use my_hdlc::pc_command::ManualInput;
 use my_hdlc::HdlcTransceiver;
+use tudelft_quadrupel::block;
 use tudelft_quadrupel::mpu::{
-    read_raw,
+    read_dmp_bytes, read_raw,
     structs::{Accel, Gyro},
 };
 
@@ -20,9 +22,14 @@ pub struct FSMCalibration {}
 impl FSMControl for FSMCalibration {
     fn run_state_loop(mut self: Box<Self>, ctx: &mut StateContext) -> Box<dyn FSMControl> {
         let (accel, gyro) = read_raw().unwrap();
-
+        let dmp_data = block!(read_dmp_bytes());
+        let ypr = if (dmp_data.is_ok()) {
+            YawPitchRoll::from(dmp_data.unwrap())
+        } else {
+            YawPitchRoll::new()
+        };
         // read new sample
-        ctx.calibration_state.read_new_sample(accel, gyro);
+        ctx.calibration_state.read_new_sample(accel, gyro, ypr);
 
         if ctx.calibration_state.should_finish() {
             ctx.calibration_state.finalize_calibration();
